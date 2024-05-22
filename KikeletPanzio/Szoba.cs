21fc50a8da1684d1f71e05f43ac8c7d7323cd58d
Laci <1sikelaszlo@gmail.com>
2024-05-22 09:13:23 +0200

    Szoba class

---
+++ KikeletPanzio/Szoba.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KikeletPanzio
+{
+    internal class Szoba
+    {
+        string szobaszam;
+        int ferohely;
+        int ar;
+
+        public Szoba(string szobaszam, int ferohely, int ar)
+        {
+            Szobaszam = szobaszam;
+            Ferohely = ferohely;
+            Ar = ar;
+        }
+
+        public string Szobaszam { get => szobaszam; set => szobaszam = value; }
+        public int Ferohely { get => ferohely; set => ferohely = value; }
+        public int Ar { get => ar; set => ar = value; }
+
+        public Szoba(string sor)
+        {
+            string[] bontas = sor.Split(';');
+            Szobaszam = bontas[0];
+            Ferohely = int.Parse(bontas[1]);
+            Ar = int.Parse(bontas[2]);
+        }
+
+        public override string ToString()
+        {
+            return $"{Szobaszam};{Ferohely};{Ar}";
+        }
+    }
+}
